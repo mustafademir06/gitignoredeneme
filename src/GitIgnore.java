ef2d6b07384d3afe7ea199dc8dd6b6949ca273fa
Mustafa Demir <mustafade@gmail.com>
2022-08-16 23:10:10 +0300

    -CC2

--- src/GitIgnore.java
+++ src/GitIgnore.java
@@ -3,6 +3,7 @@ public class GitIgnore {
         System.out.println("Gitignore deneme 1");
         System.out.println("Gitignore deneme 2");
         System.out.println("Gitignore deneme Branch");
+        System.out.println("Gitignore deneme master");
 
     }
 }
