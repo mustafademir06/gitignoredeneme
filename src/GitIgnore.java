9c58d14f2fdead4a32bc53a237ac34a08419c313
Mustafa Demir <mustafade@gmail.com>
2022-08-16 23:00:19 +0300

    QWER

--- src/GitIgnore.java
+++ src/GitIgnore.java
@@ -1,5 +1,7 @@
 public class GitIgnore {
     public static void main(String[] args) {
         System.out.println("Gitignore deneme 1");
+        System.out.println("Gitignore deneme 2");
+
     }
 }
