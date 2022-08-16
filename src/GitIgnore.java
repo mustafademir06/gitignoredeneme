166cbbf5e8e3c528210c45ef5d2cd9e5b4eabf5d
Mustafa Demir <mustafade@gmail.com>
2022-08-16 22:54:41 +0300

    pooop

--- src/GitIgnore.java
+++ src/GitIgnore.java
@@ -1,5 +1,6 @@
 public class GitIgnore {
     public static void main(String[] args) {
         System.out.println("Gitignore deneme 1");
+        System.out.println("Gitignore deneme Branch");
     }
 }
